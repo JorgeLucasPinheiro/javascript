9f2166747b8353ab4e3e5c51f95793d62a87719a
Jorge_Lucas <105086256+JorgeLucasPinheiro@users.noreply.github.com>
2022-08-09 19:30:47 -0300

    update

--- ex/ex007.js
+++
@@ -1,6 +0,0 @@
-var vel = 50
-console.log('A velocidade do seu carro é de '+ vel + ' km/h')
-if (vel > 60) { //condição simples
-    console.log('Você ultrapassou a velocidade maxima e foi multado')
-}
-console.log('Dirija sempre usando o cinto')
---
+++ ex/for.js
@@ -0,0 +1,5 @@
+console.log('Vai começar...')
+for(var c = 0 ; c <= 10 ; c = c + 2 ){
+    console.log(c)
+}
+console.log('Fim')
