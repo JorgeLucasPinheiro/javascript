54d9513b342daa21d74407bdd6c7c0a71f7cb13d
Jorge_Lucas <105086256+JorgeLucasPinheiro@users.noreply.github.com>
2022-08-10 16:44:28 -0300

    create now

---
+++ ex02/contador/script.js
@@ -0,0 +1,32 @@
+function verificar(){
+    var inicio = document.getElementById('txtinicio')
+    var fim = document.getElementById('txtfim')
+    var passo = document.getElementById('txtpasso')
+    var resultado = document.getElementById('res')
+    
+    if (inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
+        window.alert('[ERRO]')
+        resultado.innerHTML = 'Impossivel Contar'
+    } else {
+        resultado.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0){
+            alert('passo invalido Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+            for (var c = i; c <= f; c += p) {
+                resultado.innerHTML += ' ' + c + ' \u{1F449}'
+            }
+        } else {
+            //contagem regressiva
+            for (var c = i; c >= f; c -= p) {
+                resultado.innerHTML += ' ' + c + ' \u{1F449}'
+            }
+        }
+        resultado.innerHTML += '\u{1F3C1}'
+    } 
+}
